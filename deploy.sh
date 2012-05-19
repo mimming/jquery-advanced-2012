aa312a86e726acd1ff98265405165b11297d934c
Jennifer Murphy <mimming@mimming-macbookpro.(none)>
2012-05-19 08:34:46 -0700

    missed some files before

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 rm bd_advjquery_archive.zip
-zip -r bd_advjquery_archive.zip ./baking-disasters
+zip -r bd_advjquery_archive.zip ./
 scp bd_advjquery_archive.zip bakingdisasters.com:~/bakingdisasters.com/jqueryadvanced/bd_advjquery_archive.zip
 
 ssh bakingdisasters.com 'mkdir ~/bakingdisasters.com/jqueryadvanced'
-rsync -vaz ./baking-disasters/* bakingdisasters.com:~/bakingdisasters.com/jqueryadvanced/
+rsync -vaz ./baking-disasters bakingdisasters.com:~/bakingdisasters.com/jqueryadvanced/
+rsync -vaz ./derby-gallery bakingdisasters.com:~/bakingdisasters.com/jqueryadvanced/
+
